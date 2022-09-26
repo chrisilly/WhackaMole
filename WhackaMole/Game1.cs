c47f1e846fdec972412584260818f6e3a25072f8
Christian Culling <christian@culling.se>
2022-09-26 12:23:31 +0200

    Loaded assets, added Mole class

--- WhackaMole/Game1.cs
+++ WhackaMole/Game1.cs
@@ -21,6 +21,16 @@ namespace WhackaMole
 
         GameState currentGameState;
         GameState GameOver = GameState.Win | GameState.Lose;
+
+        Texture2D textureBackground;
+        Texture2D textureMole;
+        Texture2D textureMoleHit;
+        Texture2D textureHole;
+        Texture2D textureHoleForeground;
+        Texture2D textureMallet;
+
+        Mole[,] moles = new Mole[3, 3];
+        Hole[,] moleHoles = new Hole[3, 3];
         
 
         public Game1()
@@ -45,6 +55,12 @@ namespace WhackaMole
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            textureMole = Content.Load<Texture2D>("mole");
+            textureHole = Content.Load<Texture2D>("hole");
+            textureHoleForeground = Content.Load<Texture2D>("hole_foreground");
+            textureMallet = Content.Load<Texture2D>("mallet");
+            
+            PopulateMoles();
         }
 
         protected override void Update(GameTime gameTime)
@@ -59,11 +75,31 @@ namespace WhackaMole
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LawnGreen); // HoleForeground color is (111, 209, 72)
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
 
+            foreach (Mole mole in moles)
+            {
+                mole.Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        public void PopulateMoles()
+        {
+            int marginMoles = 250;
+
+            for (int i = 0; i < moles.GetLength(0); i++)
+            {
+                for (int j = 0; j < moles.GetLength(1); j++)
+                {
+                    moles[i, j] = new Mole(i*marginMoles, j*marginMoles, textureMole);
+                }
+            }
         }
     }
 }
---
+++ WhackaMole/Mole.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WhackaMole
+{
+    internal class Mole
+    {
+        Texture2D textureHole;
+        Texture2D textureHoleForeground;
+
+        Texture2D textureMole;
+        Texture2D textureMoleHit;
+        Vector2 positionMole;
+        Vector2 velocityMole;
+
+        // Lonesome mole, do not use
+        public Mole(int positionMoleX, int positionMoleY, Texture2D textureMole)
+        {
+            this.textureMole = textureMole;
+            this.positionMole = new Vector2(positionMoleX, positionMoleY);
+        }
+
+        // Generate a Mole including the hole and holeForeground textures bundled into one object
+        public Mole(int positionMoleX, int positionMoleY, Texture2D textureMole, Texture2D textureHole, Texture2D textureHoleForeground)
+        {
+            this.textureMole = textureMole;
+            this.textureHole = textureHole;
+            this.textureHoleForeground = textureHoleForeground;
+            this.positionMole = new Vector2(positionMoleX, positionMoleY);
+
+        }
+
+        public Mole(Texture2D textureMole)
+        {
+            this.textureMole = textureMole;
+            velocityMole = Vector2.Zero;
+        }
+        
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textureMole, positionMole, Color.White);
+        }
+    }
+}
